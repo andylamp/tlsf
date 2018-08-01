13c3fdecfe54c9ad7542f1ff75d7bad50d0383be
Periklis Akritidis <akritid@niometrics.com>
2018-08-01 18:26:18 +0300

    ASAN instrumentation and store heap in metadata

--- example.c
+++ example.c
@@ -19,6 +19,13 @@ main(int argc, char **argv)
 	tlsf_t *tlsf = tlsf_create(ctrl);
 	pool_t *pool = tlsf_add_pool(tlsf, memory, memory_size);
 
+	printf("tlsf_size()=%zu\n", tlsf_size());
+	printf("tlsf_align_size()=%zu\n", tlsf_align_size());
+	printf("tlsf_block_size_min()=%zu\n", tlsf_block_size_min());
+	printf("tlsf_block_size_max()=%zu\n", tlsf_block_size_max());
+	printf("tlsf_pool_overhead()=%zu\n", tlsf_pool_overhead());
+	printf("tlsf_alloc_overhead()=%zu\n", tlsf_alloc_overhead());
+
 	void *table[TABLE_SIZE] = { 0 };
 
 	for (int i = 0; i < ITERATIONS; i++) {
@@ -28,7 +35,14 @@ main(int argc, char **argv)
 			memset(table[j], 0, tlsf_block_size(table[j]));
 			tlsf_free(tlsf, table[j]);
 		}
-		void *ptr = tlsf_malloc(tlsf, size);
+		void *ptr;
+		if (i % 10 == 0) {
+			int align_shift = 3 + rand() % 8;
+			size_t align = (1<<align_shift);
+			ptr = tlsf_memalign(tlsf, align, size);
+		} else {
+			ptr = tlsf_malloc(tlsf, size);
+		}
 		if (ptr == NULL) {
 			oom_count++;
 		} else {
