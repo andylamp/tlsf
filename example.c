3cf514a86ca8da7a769afbfacfab5d9c31d80d70
Periklis Akritidis <akritid@niometrics.com>
2018-08-01 16:07:04 +0300

    Add stress test in example.c

--- example.c
+++ example.c
@@ -1,29 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "tlsf.h"
+
+#define TABLE_SIZE 1000000
+#define ITERATIONS 10000000
+#define MAX_SIZE 5000
 
 int
 main(int argc, char **argv)
 {
-	size_t memory_size = 1024*1024;
+	size_t memory_size = 1000000000;
 	char *memory = malloc(memory_size);
+	char ctrl[tlsf_size()];
+	int oom_count = 0;
 
-	tlsf_t *tlsf = tlsf_create_with_pool(memory, memory_size);
+	tlsf_t *tlsf = tlsf_create(ctrl);
+	pool_t *pool = tlsf_add_pool(tlsf, memory, memory_size);
 
-	void *p1 = tlsf_malloc(tlsf, 100);
-	void *p2 = tlsf_malloc(tlsf, 1000);
-	tlsf_free(tlsf, p2);
-	void *p3 = tlsf_malloc(tlsf, 1000);
+	void *table[TABLE_SIZE] = { 0 };
 
-	printf("Allocated %p\n", p1);
-	printf("Allocated %p\n", p2);
-	printf("Allocated %p\n", p3);
+	for (int i = 0; i < ITERATIONS; i++) {
+		int j = rand() % TABLE_SIZE;
+		size_t size = rand() % (MAX_SIZE + 1);
+		if (table[j] != NULL) {
+			memset(table[j], 0, tlsf_block_size(table[j]));
+			tlsf_free(tlsf, table[j]);
+		}
+		void *ptr = tlsf_malloc(tlsf, size);
+		if (ptr == NULL) {
+			oom_count++;
+		} else {
+			memset(ptr, 0, size);
+		}
+		table[j] = ptr;
 
-	tlsf_free(tlsf, p3);
-	tlsf_free(tlsf, p1);
+	}
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		if (table[i] != NULL) {
+			tlsf_free(tlsf, table[i]);
+			table[i] = NULL;
+		}
+	}
+
+	tlsf_remove_pool(tlsf, pool);
 
 	tlsf_destroy(tlsf);
+
+	memset(memory, 0, memory_size);
+	printf("memory: %p\n", memory);
+	printf("oom count: %d\n", oom_count);
 
 	free(memory);
 }
