427524ae39c937507309915e3c83f6c82684cd3a
Periklis Akritidis <akritid@niometrics.com>
2018-08-01 17:38:03 +0300

    Add ASAN support

---
+++ asan.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// __has_feature is clang-only
+#if !defined(__has_feature)
+#define __has_feature(FEATURE) 0
+#endif
+
+#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+#include <sanitizer/asan_interface.h>
+
+// Set to 1 to enable tracing on asan poisoning calls
+#if 0
+#define PTR_ADD(p, x) ((void *)((uintptr_t)p + (x)))
+#undef ASAN_POISON_MEMORY_REGION
+#define ASAN_POISON_MEMORY_REGION(addr, size) do { \
+	__asan_poison_memory_region((addr), (size)); \
+	fprintf(stderr, "%s:   poison %p - %p\n", __func__, (addr), PTR_ADD((addr), (size))); \
+} while (0)
+#undef ASAN_UNPOISON_MEMORY_REGION
+#define ASAN_UNPOISON_MEMORY_REGION(addr, size) do { \
+	__asan_unpoison_memory_region((addr), (size)); \
+	fprintf(stderr, "%s: unpoison %p - %p\n", __func__, (addr), PTR_ADD((addr), (size))); \
+} while (0)
+#endif
+#else
+#define ASAN_POISON_MEMORY_REGION(addr, size) \
+	((void)(addr), (void)(size))
+#define ASAN_UNPOISON_MEMORY_REGION(addr, size) \
+	((void)(addr), (void)(size))
+#endif /* address_sanitizer */
